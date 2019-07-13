b2f05860f692783ebbecdfd072f2a35569ae34b8
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-12 21:48:49 -0400

    Simplify design and description

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 /*
-A customer appears on one of the barstools at a random interval.
-The user clicks the customer and a popup appears with his order.
-The user clicks a button to close the popup and a timer begins.
-The timer measures how long the customer has had to wait for his drink.
-The customer begins green, then turns yellow, red, and then leaves if too much time has passed.
-The user may click on the Mixology Manual in the nav bar at any time to look up a drink.
-The manual is a drop-down menu; if the user clicks a drink [array of objects], a window opens with the recipe.
 Each drink (class) has a glass-type, ice or no ice, a number of oz for the spirit, a number of oz for the mixer, maybe a garnish, and a price.
 The user clicks the glass type to select it and it appears on the staging area.
 If the user clicks ice, it appears in the glass and it makes a clinking sound.
@@ -18,4 +11,24 @@ The customer and the drink both disappear and a tip is calculated.
 The tip is a percentage depending on the price of the drink and the happiness of the customer.
 The user can see the total tips and total drinks served on the nav bar.
 A ratio of these two factors determines whether the user is ready for the next level.
+*/
+/* Even Simpler Version:
+Click a button that says get order.
+class Drink() {
+    name:
+    glass:
+    ice: (boolean)
+    
+}
+A Drink from an array is displayed.
+Click a glass.
+Click ice.
+Click a spirit.
+Click a mixer.
+Click a garnish.
+if(drinkServed === drinkOrdered) {
+    return "Thanks!"
+} else {
+    return "You got my drink wrong."
+};
 */
