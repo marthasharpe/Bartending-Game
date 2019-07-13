80f33cf4a4deb54c1a6bc4eb6a21fff5b80c1b7d
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-12 22:09:46 -0400

    Begin translating the user stories into code

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-/*
-Each drink (class) has a glass-type, ice or no ice, a number of oz for the spirit, a number of oz for the mixer, maybe a garnish, and a price.
-The user clicks the glass type to select it and it appears on the staging area.
-If the user clicks ice, it appears in the glass and it makes a clinking sound.
-The user clicks on a spirit bottle (1 click = 1 oz) and it makes a pouring sound.
-The user clicks on a mixer bottle (1 click = 1 oz) and it makes a pouring sound.
-The user clicks a garnish if needed and it appears on the drink.
-The user clicks the counter in front of the customer to serve the drink.
-If the user got the recipe wrong, it will effect the customer's happiness level.
-The customer and the drink both disappear and a tip is calculated.
-The tip is a percentage depending on the price of the drink and the happiness of the customer.
-The user can see the total tips and total drinks served on the nav bar.
-A ratio of these two factors determines whether the user is ready for the next level.
-*/
-/* Even Simpler Version:
-Click a button that says get order.
+/* Simplified Version
+
 class Drink() {
-    name:
-    glass:
-    ice: (boolean)
-    
+    constructor(name, glass, ice, spirit, mixer, garnish) {
+        this.name = name;
+        this.glass = glass;
+        this.ice = ice;
+        this.spirit = spirit;
+        this.mixer = mixer;
+        this.garnish = garnish;
+    }
 }
-A Drink from an array is displayed.
-Click a glass.
-Click ice.
-Click a spirit.
-Click a mixer.
-Click a garnish.
-if(drinkServed === drinkOrdered) {
-    return "Thanks!"
+
+const rumCoke = new Drink('Rum and Coke', )
+
+const drinkArray = [rumCoke, scotchSoda, whiskeySour, ginTonic];
+
+function takeOrder(e) {
+    alert `I want a ${Drink}.`
+}
+
+const order = document.addEventListener('click', takeOrder);
+
+
+onClick add items to drinkServed array.
+
+Which glass?
+[]
+Ice or no ice?
+[]
+Which spirit?
+[]
+Which mixer(s)?
+[]
+Add a garnish?
+[]
+
+if (drinkServed === drinkOrdered) {
+    alert "Thanks! Keep the change."
 } else {
-    return "You got my drink wrong."
-};
+    alert "You got my drink wrong."
+}
+
 */
