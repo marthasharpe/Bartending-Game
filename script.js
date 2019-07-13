0fccd46466bc20eb8dbbdb2a76e57b460ba533dc
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-13 15:20:38 -0400

    Execute takeOrder function on click

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Drink {
     }
 }
 
+/*List of Drinks*/
 const rumCoke = new Drink('Rum and Coke', 'rum', 'coke', 'lemon');
 const ginTonic = new Drink('Gin and Tonic', 'gin', 'tonic', 'lime');
 const scotchSoda = new Drink('Scotch and Soda', 'scotch', 'soda', 'none');
@@ -14,20 +15,17 @@ const screwdriver = new Drink('Screwdriver', 'tequila', 'orange juice', 'orange'
 const whiskeySour = new Drink('Whiskey Sour', 'bourbon', 'sour mix', 'cherry');
 
 const drinkArray = [rumCoke, ginTonic, scotchSoda, screwdriver, whiskeySour];
-console.log(rumCoke);
-console.log(drinkArray);
-
 const drinkOrder = drinkArray[Math.floor (Math.random() * drinkArray.length)];
-console.log(drinkOrder);
 
-/*
-function drinkOrder(drinkArray) {
-    return ;
-    console.log(`I want a ${Drink.name}.`)
+function takeOrder() {
+    console.log(`I want a ${drinkOrder.name}.`)
 }
 
+function mixDrink() {
+    
+}
 
-const order = document.addEventListener('click', drinkOrder);
+/*addEventListener('click', drinkOrder);
 
 onClick add items to drinkServed array.
 
@@ -44,4 +42,5 @@ if (drinkServed === drinkOrder) {
     alert "You got my drink wrong."
 }
 
+Optional feature: 'Get a Hint' button. Lists the attributes of the drinkOrder.
 */
