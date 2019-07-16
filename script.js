744af8cd48f4bb513b7146bf1a69084ce38b94b2
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-16 14:57:08 -0400

    Reset drinkOrder and newDrink when drink is served

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
-// Drinks
+// Drink menu
+
 const shotRum = { 
     name: 'shot of Rum',
     glass: 'Shot', 
     spirit: 'Rum',
 };
+
 const shotGin = {
     name: 'shot of Gin',
     glass: 'Shot',
     spirit: 'Gin',
-}
+};
 
 const drinkArray = [shotRum, shotGin];
-const glassArray = ['Shot']
+const glassArray = ['Shot'];
 const spiritArray = ['Rum', 'Gin'];
 
 let drinkOrder = {};
+let newDrink = {};
 
 function takeOrder() {
     //Generate random drinkOrder from drinkArray onClick.
     drinkOrder = drinkArray[Math.floor (Math.random() * drinkArray.length)];
-    console.log(`I want a ${drinkOrder.name}.`)
-    // can change the text on the button or add a popup
-}
+    console.log(`I want a ${drinkOrder.name}.`);
+};
 
-let glassName = glassArray[0];
-let glassItem = document.createElement('li');
-glassItem.innerText = glassName;
-glassItem.className = 'glass'; //Each <li> has class="glass"
-document.getElementsByClassName('glass-selection')[0].appendChild(glassItem);
-glassItem.addEventListener('click', () => {
-    drinkServed.glass = glassName;
-});
+function serveDrink() {
+    if (newDrink.glass === drinkOrder.glass && newDrink.spirit === drinkOrder.spirit) {
+        console.log("Thanks! Keep the change.")
+        drinkOrder = {};
+        newDrink = {};
+    } else {
+        console.log("You got my drink wrong.")
+        newDrink = {};
+    }
+    console.log(newDrink);
+    console.log(drinkOrder);
+};
+
+for(let i = 0; i < glassArray.length; i++) {
+    let glassName = glassArray[i];
+    let glassItem = document.createElement('li');
+    glassItem.innerText = glassName;
+    glassItem.className = 'glass'; //Each <li> has class="glass"
+    document.getElementsByClassName('glass-selection')[0].appendChild(glassItem);
+    glassItem.addEventListener('click', () => {
+        newDrink.glass = glassName;
+    });
+};
 
 //Dynamically populate HTML unordered list with class="spirit-selection" with list items.
 for(let i = 0; i < spiritArray.length; i++) {
@@ -38,22 +55,10 @@ for(let i = 0; i < spiritArray.length; i++) {
     let spiritItem = document.createElement('li');
     spiritItem.innerText = spiritName;
     spiritItem.className = 'spirit'; //Each <li> has class="spirit" 
-    //Puts <li> elements in <ul> with class="spirit-selection"
     document.getElementsByClassName('spirit-selection')[0].appendChild(spiritItem);
     spiritItem.addEventListener('click', () => {
-        drinkServed.spirit = spiritName;
+        newDrink.spirit = spiritName;
     });
-}
-
-let drinkServed = {};
-
-function serveDrink() {
-    if (drinkServed.glass === drinkOrder.glass && drinkServed.spirit === drinkOrder.spirit) {
-        console.log("Thanks! Keep the change.")
-    } else {
-        console.log("You got my drink wrong.")
-    }
-    console.log(drinkServed);
 };
 
 //Optional feature: 'Get a Hint' button. Lists the attributes of the drinkOrder.
