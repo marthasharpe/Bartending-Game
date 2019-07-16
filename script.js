b9b0409325151698570aab19d6afad70236653e5
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-16 15:32:46 -0400

    Add two new drinks to menu

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ const shotGin = {
     spirit: 'Gin',
 };
 
-const drinkArray = [shotRum, shotGin];
+const rumCoke = {
+    name: 'Rum and Coke',
+    glass: 'Highball',
+    spirit: 'Rum',
+    mixer: 'Coke',
+}
+
+const ginTonic = {
+    name: 'Gin and Tonic',
+    glass: 'Highball',
+    spirit: 'Gin',
+    mixer: 'Tonic',
+}
+
+const drinkArray = [shotRum, shotGin, rumCoke, ginTonic];
 const glassArray = ['Shot', 'Highball'];
 const spiritArray = ['Rum', 'Gin'];
 const mixerArray = ['Coke', 'Tonic'];
@@ -25,13 +39,13 @@ function takeOrder() {
 };
 
 function serveDrink() {
+    console.log(newDrink);
     if (newDrink.glass === drinkOrder.glass && newDrink.spirit === drinkOrder.spirit && newDrink.mixer === drinkOrder.mixer) {
         console.log("Thanks! Here's your tip!")
         drinkOrder = drinkArray[Math.floor (Math.random() * drinkArray.length)];
         newDrink = {};
     } else {
         console.log("You got my drink wrong. Try again.")
-        console.log(newDrink);
         newDrink = {};
     }
 
